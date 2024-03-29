c57dd051ce6b47f9a4a62a9ddbe01fa37a9f994c
Pedro-V <mingas62@hotmail.com>
2024-03-29 20:44:18 -0300

    Removed useless variable.

--- src/Direction.cpp
+++ src/Direction.cpp
@@ -32,8 +32,7 @@ std::tuple<int, int> advanceDir(int x, int y, Direction dir, int scale) {
         {Direction::NorthWest, {-1, 1}}
     };
 
-    auto inc = Increments[dir];
-    auto [x_increment, y_increment] = inc;
+    auto [x_increment, y_increment] = Increment[dir];
     return std::make_tuple(x + scale * x_increment, y + scale * y_increment);
 }
 
